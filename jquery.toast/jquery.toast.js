85088033f571c604422681662ee98bffb2aff853
Brian Maxwell <bdmaxwell@gmail.com>
2013-04-26 11:24:27 -0400

    Small tweaks.
    Updated the example page.
    Fixed a few code inconsistencies.
    Changed 'closeForStickyOnly' option to default to true.
    Minor css change.

--- jquery.toast/jquery.toast.js
+++ jquery.toast/jquery.toast.js
@@ -1,23 +1,20 @@
 (function($){
 	var th = null, cf = null, toast = function(m,o){
 		// fix option type
-		o = o || { duration: 5000, sticky: false, 'type': ''};
-		(typeof o.duration === 'number') || (o.duration = 5000);
-		(typeof o.sticky === 'boolean') || (o.sticky = true);
-		(typeof o.type === 'string') || (o.type = '');
+		o = $.extend({ duration: 5000, sticky: false, 'type': ''}, o);
+		typeof o.duration === 'number' || (o.duration = 5000);
+		typeof o.sticky === 'boolean' || (o.sticky = false);
+		typeof o.type === 'string' || (o.type = '');
 		// create host on first call
 		if(!th){ 
 			// get/fix config
 			cf = toast.config;
 			th = $('<ul></ul>').addClass('toast').appendTo(document.body).hide();
-			(typeof cf.width == 'number') || (cf.width = 500);
-			(typeof cf.align == 'string') || (cf.align = 'center');
-			(typeof cf.closeForStickyOnly === 'boolean') || (cf.closeForStickyOnly = false);
+			typeof cf.width === 'number' || (cf.width = 500);
+			typeof cf.align === 'string' || (cf.align = 'center');
+			typeof cf.closeForStickyOnly === 'boolean' || (cf.closeForStickyOnly = false);
 			th.width(cf.width);
-			if(cf.align == 'left' || cf.align == 'right')
-				th.css('margin','5px').css(cf.align, '0');
-			else
-				th.css({left: '50%', margin: '5px 0 0 -' + (cf.width / 2) + 'px'});
+			(cf.align === 'left' || cf.align === 'right') && th.css('margin','5px').css(cf.align, '0') || th.css({left: '50%', margin: '5px 0 0 -' + (cf.width / 2) + 'px'});
 		}
 		// create toast
 		var ti = $('<li></li>').hide().html(m).appendTo(th), cb = $('<button>&times;</button>').addClass('close').prependTo(ti), to = null;
@@ -31,7 +28,7 @@
 		});
 		cf.closeForStickyOnly && !o.sticky && cb.hide();
 		// add type class
-		if(o.type !== '') ti.addClass(o.type);
+		o.type !== '' && ti.addClass(o.type);
 		// show host if necessary
 		!th.hasClass('active') && th.addClass('active').show();
 		// setup timeout unless sticky
@@ -39,6 +36,6 @@
 		// show toast
 		ti.fadeIn('normal');
 	};
-	toast.config = { width: 500, align: 'center', /* left, center, right */ closeForStickyOnly: false };
+	toast.config = { width: 500, align: 'center', closeForStickyOnly: true };
 	$.extend({ toast: toast });
 })(jQuery);
